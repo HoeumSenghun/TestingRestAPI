2fad8295942f6763d343bfc4ea1debbb409d40c8
HoeumSenghun <hoeumsenghun369@gmail.com>
2025-03-11 23:50:42 +0700

    init commit

---
+++ src/test/java/com/restapi/testingrestapi/TestingRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.testingrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
